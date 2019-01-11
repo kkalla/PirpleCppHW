08872962c275fa8c8c7c806b1d97a2fb7a649140
An-joon Lee <dkswns333@gmail.com>
2019-01-11 16:14:22 +0900

    Add files via upload

---
+++ Homework #1/GreenBottles/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int greenBottles = 10;
+    cout << "There were " << greenBottles << " green Bottles" << endl;
+    greenBottles--;
+    cout << "There were " << greenBottles << " green Bottles" << endl;
+
+    return 0;
+}
